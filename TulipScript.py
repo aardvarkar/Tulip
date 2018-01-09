1d46895ed6f21e3d8a2eb8cc1970b4e6832a171c
Adrien Rohan <adrien.rohan@hotmail.fr>
2018-01-09 10:11:15 +0100

    Prmier commit

---
+++ TulipScript.py
@@ -0,0 +1,108 @@
+# Powered by Python 2.7
+
+# To cancel the modifications performed by the script
+# on the current graph, click on the undo button.
+
+# Some useful keyboards shortcuts : 
+#   * Ctrl + D : comment selected lines.
+#   * Ctrl + Shift + D  : uncomment selected lines.
+#   * Ctrl + I : indent selected lines.
+#   * Ctrl + Shift + I  : unindent selected lines.
+#   * Ctrl + Return  : run script.
+#   * Ctrl + F  : find selected text.
+#   * Ctrl + R  : replace selected text.
+#   * Ctrl + Space  : show auto-completion dialog.
+
+from tulip import tlp
+import random
+
+# The updateVisualization(centerViews = True) function can be called
+# during script execution to update the opened views
+
+# The pauseScript() function can be called to pause the script execution.
+# To resume the script execution, you will have to click on the "Run script " button.
+
+# The runGraphScript(scriptFile, graph) function can be called to launch
+# another edited script on a tlp.Graph object.
+# The scriptFile parameter defines the script name to call (in the form [a-zA-Z0-9_]+.py)
+
+# The main(graph) function must be defined 
+# to run the script on the current graph
+
+def pretraitement(graph, Locus, Negative, Positive, viewBorderColor, viewLabel, viewLayout, viewSize):
+  for n in graph.getNodes():
+    print(n)
+   
+  size = 1000
+  for n in graph.getNodes():
+    x = random.random() * size
+    y = random.random() * size
+    viewLayout[n] = tlp.Coord(x, y, 0)
+    viewSize[n] = tlp.Size(10,10,10)
+    viewLabel[n] = Locus[n]
+  for n in graph.getEdges():
+    if Negative[n] == True:
+      if Positive[n] == True:
+        viewBorderColor[n] = tlp.Color.Blue
+      else:
+        viewBorderColor[n] = tlp.Color.Green
+    elif Positive[n] == True:
+      viewBorderColor[n] = tlp.Color.Red
+    else:
+      viewBorderColor[n] = tlp.Color.Violet
+ 
+def partitionnement1(working, tp17_s):
+  for n in graph.getEdges():
+    poids = abs(tp17_s[graph.source(n)]-tp17_s[graph.target(n)])
+    print(poids)
+
+def main(graph): 
+  Locus = graph.getStringProperty("Locus")
+  Negative = graph.getBooleanProperty("Negative")
+  Positive = graph.getBooleanProperty("Positive")
+  tp1_s = graph.getDoubleProperty("tp1 s")
+  tp10_s = graph.getDoubleProperty("tp10 s")
+  tp11_s = graph.getDoubleProperty("tp11 s")
+  tp12_s = graph.getDoubleProperty("tp12 s")
+  tp13_s = graph.getDoubleProperty("tp13 s")
+  tp14_s = graph.getDoubleProperty("tp14 s")
+  tp15_s = graph.getDoubleProperty("tp15 s")
+  tp16_s = graph.getDoubleProperty("tp16 s")
+  tp17_s = graph.getDoubleProperty("tp17 s")
+  tp2_s = graph.getDoubleProperty("tp2 s")
+  tp3_s = graph.getDoubleProperty("tp3 s")
+  tp4_s = graph.getDoubleProperty("tp4 s")
+  tp5_s = graph.getDoubleProperty("tp5 s")
+  tp6_s = graph.getDoubleProperty("tp6 s")
+  tp7_s = graph.getDoubleProperty("tp7 s")
+  tp8_s = graph.getDoubleProperty("tp8 s")
+  tp9_s = graph.getDoubleProperty("tp9 s")
+  viewBorderColor = graph.getColorProperty("viewBorderColor")
+  viewBorderWidth = graph.getDoubleProperty("viewBorderWidth")
+  viewColor = graph.getColorProperty("viewColor")
+  viewFont = graph.getStringProperty("viewFont")
+  viewFontSize = graph.getIntegerProperty("viewFontSize")
+  viewIcon = graph.getStringProperty("viewIcon")
+  viewLabel = graph.getStringProperty("viewLabel")
+  viewLabelBorderColor = graph.getColorProperty("viewLabelBorderColor")
+  viewLabelBorderWidth = graph.getDoubleProperty("viewLabelBorderWidth")
+  viewLabelColor = graph.getColorProperty("viewLabelColor")
+  viewLabelPosition = graph.getIntegerProperty("viewLabelPosition")
+  viewLayout = graph.getLayoutProperty("viewLayout")
+  viewMetric = graph.getDoubleProperty("viewMetric")
+  viewRotation = graph.getDoubleProperty("viewRotation")
+  viewSelection = graph.getBooleanProperty("viewSelection")
+  viewShape = graph.getIntegerProperty("viewShape")
+  viewSize = graph.getSizeProperty("viewSize")
+  viewSrcAnchorShape = graph.getIntegerProperty("viewSrcAnchorShape")
+  viewSrcAnchorSize = graph.getSizeProperty("viewSrcAnchorSize")
+  viewTexture = graph.getStringProperty("viewTexture")
+  viewTgtAnchorShape = graph.getIntegerProperty("viewTgtAnchorShape")
+  viewTgtAnchorSize = graph.getSizeProperty("viewTgtAnchorSize")
+
+  #
+  clone = graph.addCloneSubGraph("Clone")
+  working = graph.addCloneSubGraph("Working")
+  print(Locus)
+  pretraitement(working, Locus, Negative, Positive, viewColor, viewLabel, viewLayout, viewSize)
+  partitionnement1(working, tp17_s)
