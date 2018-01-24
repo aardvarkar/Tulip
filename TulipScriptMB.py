7245558983d5aaf3187afbc67a806fd24a9e3b0b
Martin Binet <martin.binet@laposte.net>
2018-01-24 09:14:06 +0100

    multiniveau heatmap

--- TulipScriptMB.py
+++ TulipScriptMB.py
@@ -14,7 +14,10 @@
 #   * Ctrl + Space  : show auto-completion dialog.
 
 from tulip import tlp
+#import scipy
+#from scipy import stats
 import random
+import math
 
 # The updateVisualization(centerViews = True) function can be called
 # during script execution to update the opened views
@@ -34,21 +37,17 @@ import random
 def pretraitement(graph, Locus, Negative, Positive, viewBorderColor, viewLabel, viewLayout, viewSize):
   size = 1000
   for n in graph.getNodes():
-    x = random.random() * size
-    y = random.random() * size
-    viewLayout[n] = tlp.Coord(x, y, 0)
-    viewSize[n] = tlp.Size(10,3,10)
+    viewSize[n] = tlp.Size(10000,3000,10)
     viewLabel[n] = Locus[n]
   for n in graph.getEdges():
     if Negative[n] == True:
       if Positive[n] == True:
-        viewBorderColor[n] = tlp.Color.Blue
+        viewBorderColor[n] = tlp.Color.Black
       else:
-        viewBorderColor[n] = tlp.Color.Green
+        viewBorderColor[n] = tlp.Color(0,0,225)
     elif Positive[n] == True:
-      viewBorderColor[n] = tlp.Color.Red
-    else:
-      viewBorderColor[n] = tlp.Color.Violet
+      viewBorderColor[n] = tlp.Color(0,200,0)
+      
 
 def applyModelForce(graph, viewLayout):
   params = tlp.getDefaultPluginParameters('FM^3 (OGDF)', graph)
@@ -81,21 +80,108 @@ def createDistanceGraph(graph):
   tp7_s = distanceGraph.getDoubleProperty("tp7 s")
   tp8_s = distanceGraph.getDoubleProperty("tp8 s")
   tp9_s = distanceGraph.getDoubleProperty("tp9 s")
+  total_tps=[tp1_s, tp2_s, tp3_s,  tp4_s,  tp5_s, tp6_s, tp7_s, tp8_s, tp9_s, tp10_s, tp11_s, tp12_s, tp13_s, tp14_s, tp15_s, tp16_s, tp17_s]
   
   for n in distanceGraph.getEdges():
     distanceGraph.delEdge(n)
   listOfNodes=[]
   for n in distanceGraph.getNodes():
-    listOfNodes.append(n)
     expression_lvl[n] = (tp1_s[n] + tp2_s[n] + tp3_s[n] + tp4_s[n] + tp5_s[n] + tp6_s[n] + tp7_s[n] + tp8_s[n] + tp9_s[n] + tp10_s[n] + tp11_s[n] + tp12_s[n] + tp13_s[n] + tp14_s[n] + tp15_s[n] + tp16_s[n] + tp17_s[n])/17
+    if expression_lvl[n]==0:
+      distanceGraph.delNode(n)
+    else:
+      listOfNodes.append(n)
+  calculPearson(distanceGraph, listOfNodes, total_tps)
+  '''
   for i in range(len(listOfNodes)):
+    somme_i=0
+    somme_i_carre=0
+    Nb=len(total_tps)
+    for k in range(Nb):
+      value_k_of_i=total_tps[k][listOfNodes[i]]
+      somme_i=somme_i+value_k_of_i
+      somme_i_carre=somme_i_carre+math.pow(value_k_of_i,2)  
     for j in range(i+1,len(listOfNodes)):
-      distance = abs(expression_lvl[listOfNodes[i]]-expression_lvl[listOfNodes[j]])
-      if distance < 0.05 and expression_lvl[listOfNodes[i]] != 0:
+      '''
+  '''
+      distance=0
+      for k in range(len(total_tps)):
+        distance=distance+math.pow(abs(total_tps[k][listOfNodes[i]]-total_tps[k][listOfNodes[j]]),2)        
+      distance=math.sqrt(distance)
+      '''
+  '''
+      distance=calculPearson(listOfNodes, i, value_k_of_i, somme_i, somme_i_carre, j, total_tps)
+      if distance < 2:
         newEdge = distanceGraph.addEdge(listOfNodes[i], listOfNodes[j])
         poids[newEdge] = distance
+        '''
   return distanceGraph
-   
+
+'''
+def calculPearson(graph, listOfNodes, total_tps):
+  poids = graph.getDoubleProperty("Weight")  
+  Nb=len(total_tps)
+  listTpsAll=[]
+  for i in range(len(listOfNodes)):
+    listTps_i=[]
+    for k in range(Nb):
+      listTps_i.append(total_tps[k][listOfNodes[i]])
+    listTpsAll.append(listTps_i)
+  for i in range(len(listOfNodes)):
+    for j in range(i+1,len(listOfNodes)):
+      pearson=scipy.stats.pearsonr(listTpsAll[i], listTpsAll[i])[0]
+      if pearson > 0.9:
+        newEdge = graph.addEdge(listOfNodes[i], listOfNodes[j])
+        poids[newEdge] = pearson
+'''
+
+
+    
+  
+  
+
+def calculPearson(graph, listOfNodes, total_tps):
+  sommeListe=[]
+  somme_carreListe=[]
+  Nb=len(total_tps)
+  poids = graph.getDoubleProperty("Weight")
+  for i in range(len(listOfNodes)):
+    somme_i=0
+    somme_i_carre=0
+    for k in range(Nb):
+      value_k_of_i=total_tps[k][listOfNodes[i]]
+      somme_i=somme_i+value_k_of_i
+      somme_i_carre=somme_i_carre+math.pow(value_k_of_i,2)
+    sommeListe.append(somme_i)
+    somme_carreListe.append(somme_i_carre)
+  for i in range(len(listOfNodes)):
+    for j in range(i+1,len(listOfNodes)):
+      somme_produit=0
+      for k in range(Nb):
+        value_k_of_i=total_tps[k][listOfNodes[i]]
+        value_k_of_j=total_tps[k][listOfNodes[j]]
+        somme_produit=somme_produit+(value_k_of_i*value_k_of_j)      
+      denominator=math.sqrt((Nb*somme_carreListe[i]-math.pow(sommeListe[i],2)))*math.sqrt((Nb*somme_carreListe[j]-math.pow(sommeListe[j],2)))
+      if denominator==0:
+        r_value=0
+      else:
+        r_value=(Nb*somme_produit-sommeListe[i]*sommeListe[j])/denominator
+      distance=1-r_value
+      if r_value > 0.8:
+      #if distance < 0.1:
+        newEdge = graph.addEdge(listOfNodes[i], listOfNodes[j])
+        poids[newEdge] = r_value
+        #poids[newEdge] = distance
+#  count=0
+#  for n in graph.getNodes():
+#    if graph.deg(n)==0:
+#      count += 1
+#  print(graph.numberOfNodes())
+#  print(count)
+  
+
+
+'''
 def partitionnement(graph,nbPartitionMax = 1, currentPartition = 0):
   if currentPartition == nbPartitionMax:
     return None
@@ -108,7 +194,34 @@ def partitionnement(graph,nbPartitionMax = 1, currentPartition = 0):
   success = graph.applyAlgorithm('Equal Value', params)
   for sousGraph in graph.getSubGraphs():
     partitionnement(sousGraph, nbPartitionMax, currentPartition+1)
+'''
+
+def partitionnement(graph,nbPartitionMax = 1, currentPartition = 0):
+  if currentPartition == nbPartitionMax:
+    return None
+    
+  poids = graph.getDoubleProperty("Weight")
+  min_lvl = poids.getEdgeDoubleMin()
+  max_lvl = poids.getEdgeDoubleMax()
+  for edge in graph.getEdges():
+    poids[edge]=(poids[edge]-min_lvl)/(max_lvl-min_lvl)
+    
+  params=tlp.getDefaultPluginParameters('MCL Clustering')
+  params['weights'] = graph.getDoubleProperty("Weight")
+  resultMetric = graph.getDoubleProperty('resultMetric' + str(currentPartition+1))
+  success = graph.applyDoubleAlgorithm('MCL Clustering', resultMetric, params)
+  params = tlp.getDefaultPluginParameters('Equal Value', graph)
+  params['Property'] = resultMetric
+  success = graph.applyAlgorithm('Equal Value', params)
+  for sousGraph in graph.getSubGraphs():
+#    poids = sousGraph.getDoubleProperty("Weight")
+#    min_lvl = poids.getEdgeDoubleMin()
+#    max_lvl = poids.getEdgeDoubleMax()
+#    for edge in sousGraph.getEdges():
+#      poids[edge]=(poids[edge]-min_lvl)/(max_lvl-min_lvl)
   
+    partitionnement(sousGraph, nbPartitionMax, currentPartition+1)
+    
   
 # Partie 3
 
@@ -118,6 +231,9 @@ def createHeatmap(graph, distanceGraph, timePoint):
     graph.delSubGraph(heatmap)
 
   heatmap=graph.addCloneSubGraph("heatmap")
+  heatmap.clear()
+  tlp.copyToGraph(heatmap, distanceGraph)
+
   expression_lvl=heatmap.getDoubleProperty("Expression_lvl")
   tps=heatmap.getDoubleProperty("Tps")
   Locus = heatmap.getStringProperty("Locus")
@@ -126,7 +242,10 @@ def createHeatmap(graph, distanceGraph, timePoint):
   for n in heatmap.getEdges():
     heatmap.delEdge(n)
   nodesListe=[]
+  #for n in heatmap.getNodes():
+  count=0
   for n in heatmap.getNodes():
+    count=count+1
     nodesListe.append(n)
   for n in nodesListe:
     expression_lvl[n]=timePoint[0][n]
@@ -137,7 +256,7 @@ def createHeatmap(graph, distanceGraph, timePoint):
       expression_lvl[addedNodes[m]]=timePoint[m][n]
       tps[addedNodes[m]]=m+1
       Locus[addedNodes[m]]=Locus[n]
-      groupesHeatmap[addedNodes[m]]= groupes[n]
+      groupesHeatmap[addedNodes[m]]= groupesHeatmap[n]
   for n in nodesListe:
     heatmap.delNode(n)
   return heatmap
@@ -149,7 +268,7 @@ def colorHeatmap(graph):
   min_lvl = expression_lvl.getNodeDoubleMin()
   max_lvl = expression_lvl.getNodeDoubleMax()
   colorScale = tlp.ColorScale([])
-  colors = [tlp.Color.Blue, tlp.Color.Red]
+  colors = [tlp.Color.Yellow, tlp.Color.Red]
   colorScale.setColorScale(colors)
   for n in graph.getNodes():
     viewColor[n]=colorScale.getColorAtPos((expression_lvl[n]-min_lvl)/(max_lvl-min_lvl))
@@ -180,51 +299,120 @@ def construireGrille(gr):
 '''
         
 
-def construireGrille(distance, gr):
-    increment = getIncrement(distance)
+def construireGrille(distance, gr, maxDepth):
+    increment = getIncrement(distance, distance)
     print (increment)
-    
+    positionY = distance.getDoubleProperty("PositionY")
   
     layout = gr.getLayoutProperty("viewLayout")
     tps = gr.getDoubleProperty("Tps")
-    groupes = gr.getDoubleProperty("resultMetric1")
     viewSize = gr.getSizeProperty("viewSize")
     decalageX = 100
     decalageY = 1.5
-    Locus = gr.getStringProperty("Locus")
+    Locus = distance.getStringProperty("Locus")
     locusToY = {}
+    for node in distance.getNodes():
+      locusToY[Locus[node]] = positionY[node]
+    print (locusToY)
+    Locus = gr.getStringProperty("Locus")
     count = {}
     for n in gr.getNodes():
         currentLocus = Locus[n]
         viewSize[n]=tlp.Size(decalageX,decalageY,1)
         x = tps[n]
-        if currentLocus in locusToY :
-          y = locusToY[currentLocus]
-        else:
-          '''
-          print(int(groupes[n]))
-          print(tmp[int(groupes[n])])
-          print(increment[tmp[int(groupes[n])]])
-          '''
-          if groupes[n] not in count:
-            count[groupes[n]] = 0
-          y = increment[groupes[n]] + count[groupes[n]]
-          count[groupes[n]] += 1
-          locusToY[currentLocus] = y
-       
+        y = locusToY[Locus[n]]
+        print(y)
         layout[n] = tlp.Coord(x * decalageX, y * decalageY, 0)
+#        if currentLocus in locusToY :
+#          y = locusToY[currentLocus]
+#        else:
+#          '''
+#          print(int(groupes[n]))
+#          print(tmp[int(groupes[n])])
+#          print(increment[tmp[int(groupes[n])]])
+#          '''
+#          for node in distance.getNodes():
+#            if Locus[node] == currentLocus:
+#              y = positionY[node]
+#          locusToY[currentLocus] = y
+          
+#          currentDepth = 1
+#          groupe = gr.getDoubleProperty("resultMetric" + str(currentDepth))
+#          tmp = increment[groupe[n]]
+#          currentDepth += 1
+#          groupe = gr.getDoubleProperty("resultMetric" + str(currentDepth))
+#          tmp = tmp[groupe[n]]
+#          currentDepth += 1
+#          groupe = gr.getDoubleProperty("resultMetric" + str(currentDepth))
+#          tmp[groupe[n]] += 1
+#          y = tmp[groupe[n]]
+##          print(y)
+#          locusToY[currentLocus] = y
+          
+#          if groupes[n] not in count:
+#              count[groupes[n]] = {}
+#          branch = count[groupes[n]]
+#          currentDepth = 2            
+#          while currentDepth < maxDepth:
+#            groupes = gr.getDoubleProperty("resultMetric" + str(currentDepth))
+#            if groupes[n] not in branch:
+#              branch[groupes[n]] = {}
+#            branch = branch[groupes[n]]
+#          
+#          y = increment[groupes[n]] + count[groupes[n]]
+#          count[groupes[n]] += 1
+#          locusToY[currentLocus] = y
+       
         
-def getIncrement(gr, depth = 1, tmp = 0):
-      
-    increment = {}
-    for graph in gr.getSubGraphs():
-      resultMetric = graph.getDoubleProperty("resultMetric"+ str(depth))[graph.getOneNode()]
+        
+#def getIncrement(gr, depth = 1, tmp = 0):
+#      
+#    increment = {}
+#    for graph in gr.getSubGraphs():
+#      resultMetric = graph.getDoubleProperty("resultMetric"+ str(depth))[graph.getOneNode()]
+#      tmp += graph.numberOfNodes()
+#      if graph.numberOfSubGraphs() > 0:
+#        increment[resultMetric] = getIncrement(graph, depth+1, tmp)
+#      else:
+#        increment[resultMetric] = tmp
+#    return increment
+    
+def getIncrement(rootGraph, gr, tmp = 0):
+#  print("**********************")
+
+  
+  for graph in gr.getSubGraphs():
+    if graph.numberOfSubGraphs() == 0:
+#      print("there are no subgraphs")
+#      print("tmp = ", tmp)
+#      print(graph.numberOfNodes())
+      positionY = rootGraph.getDoubleProperty("PositionY")
+      y = 0
+      for n in graph.getNodes():
+        positionY[n] = tmp + y
+        y += 1
       tmp += graph.numberOfNodes()
-      if graph.numberOfSubGraphs() > 0:
-        increment[resultMetric] = getIncrement(graph, depth+1, tmp)
-      else:
-        increment[resultMetric] = tmp
-    return increment
+    else:
+#      print("there are subgraphs")
+      tmp = getIncrement(rootGraph, graph, tmp)
+  return tmp
+      
+  
+  
+#  if gr.numberOfSubGraphs() < 0:
+#    return gr.numberOfNodes()
+#  increment = {}
+#  for graph in gr.getSubGraphs():
+#    resultMetric = graph.getDoubleProperty("resultMetric"+ str(depth))[graph.getOneNode()]
+#    t = rootGraph.getDoubleProperty("resultMetric"+ str(depth))
+#    for n in graph.getNodes():
+#      t[n] = resultMetric
+#    tmp += getIncrement(rootGraph, graph, depth+1, tmp)
+#    increment[resultMetric] = 
+#    else:
+#      increment[resultMetric] = tmp
+#  return increment
+    
         
     
       
@@ -232,6 +420,7 @@ def getIncrement(gr, depth = 1, tmp = 0):
 
 #def changeGrille(graph):
   
+# MAIN
 
 def main(graph):
   #
@@ -245,6 +434,7 @@ def main(graph):
     working=graph.getSubGraph("Working")
     working.clear()
     tlp.copyToGraph(working, clone)
+#  working = graph.getSubGraph("Working")
 
   Locus = working.getStringProperty("Locus")
   Negative = working.getBooleanProperty("Negative")
@@ -290,11 +480,23 @@ def main(graph):
   viewTgtAnchorSize = working.getSizeProperty("viewTgtAnchorSize")
   timePoint = [tp1_s, tp2_s, tp3_s, tp4_s, tp5_s, tp6_s, tp7_s, tp8_s, tp9_s, tp10_s, tp11_s, tp12_s, tp13_s, tp14_s, tp15_s, tp16_s, tp17_s]
 
+  '''
+  count=0
+  for i in working.getNodes():
+    count=count+1
+    if count >100:
+      working.delNode(i)
+  '''
+
+
   pretraitement(working, Locus, Negative, Positive, viewColor, viewLabel, viewLayout, viewSize)
   applyModelForce(working, viewLayout)
   distanceGraph = createDistanceGraph(working)
-  partitionnement(distanceGraph, 2)
+  maxDepth = 3
+  partitionnement(distanceGraph, maxDepth)
   heatmap = createHeatmap(working, distanceGraph, timePoint)
   #heatmap = working.getSubGraph("heatmap")
   colorHeatmap(heatmap)
-  construireGrille(distanceGraph, heatmap)
+#  distanceGraph = working.getSubGraph("distanceGraph")
+#  heatmap = working.getSubGraph("heatmap")
+  construireGrille(distanceGraph, heatmap, maxDepth)
